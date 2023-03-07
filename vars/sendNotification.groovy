f637c9e87ebc711f4976eb0fdadbecc4af21bf45
eagunuworld <eagunuworld@gmail.com>
2023-03-06 19:16:06 -0800

    push

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -15,8 +15,9 @@ def call(String buildStatus = 'STARTED') {
  }
 
  def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+ slackSend(iconEmoji: emoji, attachments: attachments)
 
- slackSend(color: color, message: msg)
+ //slackSend(color: color, message: msg)
 
 //  attachments = [
 //     [
