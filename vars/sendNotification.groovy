c5d294c5e0b69977842a2ecea95da421b3eae9c9
eagunuworld <eagunuworld@gmail.com>
2023-03-07 19:22:31 -0800

    push

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -14,6 +14,14 @@ def call(String buildStatus = 'STARTED') {
   emoji = ':hulk:'
  }
  def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+ def attachments = [
+  [
+    text: 'I find your lack of faith disturbing!',
+    fallback: 'Hey, Vader seems to be mad at you.',
+    color: '#ff0000'
+  ]
+]
  
 slackSend(iconEmoji: emoji, attachments: attachments)
 
