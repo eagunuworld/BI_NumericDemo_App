1fbadb654c031649aeff51fd31498c7c274b7581
eagunuworld <eagunuworld@gmail.com>
2023-03-07 20:10:48 -0800

    push

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -98,7 +98,7 @@ def call(String buildStatus = 'STARTED') {
               "emoji": true
             ],
             "value": "click_me_123",
-            "url": "${applicationURL}:32564",
+            "url": "${appURL}:32564",
             "action_id": "button-action"
           ]
         ],
