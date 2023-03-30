cc3b61fc44fd96c80b137cd4e46d341ca3dfbf68
eagunuworld <eagunuworld@gmail.com>
2023-03-29 22:16:24 -0500

    push

--- west-prod-rollout.sh
+++ west-prod-rollout.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 sleep 20s
 
-if [[ $(kubectl -n prod rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n west-prod rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n prod rollout undo deploy ${deploymentName}
+    kubectl -n west-prod rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout in west is Success"
